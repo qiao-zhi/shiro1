90ade3f1ad216f87866dcdb604bcc04afe385f9b
qiaozhi <qiao_liqiang@163.com>
2017-07-29 10:56:52 +0800

    授权

--- src/cn/qlq/realm/CustomRealm.java
+++ src/cn/qlq/realm/CustomRealm.java
@@ -1,17 +1,22 @@
 package cn.qlq.realm;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 
 /**
  * 自定义realm。根据上面穿下来的token去数据库查信息，查到返回一个SimpleAuthenticationInfo，查不到返回null
-* @author: qlq
-* @date :  2017年7月28日下午8:45:52
+ * 
+ * @author: qlq
+ * @date : 2017年7月28日下午8:45:52
  */
 public class CustomRealm extends AuthorizingRealm {
 
@@ -23,8 +28,7 @@ public class CustomRealm extends AuthorizingRealm {
 
 	// 用于认证
 	@Override
-	protected AuthenticationInfo doGetAuthenticationInfo(
-			AuthenticationToken token) throws AuthenticationException {
+	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
 
 		// token是用户输入的
 		// 第一步从token中取出身份信息（token代表用户输入的传下来的信息）
@@ -32,31 +36,45 @@ public class CustomRealm extends AuthorizingRealm {
 
 		// 第二步：根据用户输入的userCode从数据库查询
 		// ....从数据库查数据
-	
 
 		// 如果查询不到返回null
-		//数据库中用户账号是zhangsansan
-		/*if(!userCode.equals("zhangsansan")){//
-			return null;
-		}*/
-		
-		
+		// 数据库中用户账号是zhangsansan
+		/*
+		 * if(!userCode.equals("zhangsansan")){// return null; }
+		 */
+
 		// 模拟从数据库查询到密码
 		String password = "111111";
 
 		// 如果查询到返回认证信息AuthenticationInfo
-		SimpleAuthenticationInfo simpleAuthenticationInfo = new SimpleAuthenticationInfo(
-				userCode, password, this.getName());
+		SimpleAuthenticationInfo simpleAuthenticationInfo = new SimpleAuthenticationInfo(userCode, password,
+				this.getName());
 
 		return simpleAuthenticationInfo;
 	}
 
 	// 用于授权
 	@Override
-	protected AuthorizationInfo doGetAuthorizationInfo(
-			PrincipalCollection principals) {
-		// TODO Auto-generated method stub
-		return null;
+	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+
+		// 从 principals获取主身份信息
+		// 将getPrimaryPrincipal方法返回值转为真实身份类型（在上边的doGetAuthenticationInfo认证通过填充到SimpleAuthenticationInfo中身份类型），
+		String userCode = (String) principals.getPrimaryPrincipal();
+
+		// 根据身份信息获取权限信息
+		// 连接数据库...
+		// 模拟从数据库获取到数据
+		List<String> permissions = new ArrayList<String>();
+		permissions.add("user:create");// 用户的创建
+		permissions.add("items:add");// 商品添加权限
+		// ....
+
+		// 查到权限数据，返回授权信息(要包括 上边的permissions)
+		SimpleAuthorizationInfo simpleAuthorizationInfo = new SimpleAuthorizationInfo();
+		// 将上边查询到授权信息填充到simpleAuthorizationInfo对象中
+		simpleAuthorizationInfo.addStringPermissions(permissions);
+
+		return simpleAuthorizationInfo;
 	}
 
 }
